4a66baf3c96be4d5ce3bac39c8ad5bb5fda94d98
AllyssonL <allyssonlingad@gmail.com>
2018-07-12 22:43:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # dog.rb
+
+#varriable types 
+  #local varriable(eg. a = 3)
+  #instance varriable(eg. @a = something)
+  
+  
+  class Dog
+  end
+  
+  #Creating a mew instances and storing them as local varriables
+  fido = 
+  snoopy = 
+  lassie = 
